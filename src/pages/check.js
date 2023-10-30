a2e590618e031c5838bb149fad1fe05c07c61080
Suchismita-Deb <suchismitadeb2000@gmail.com>
2023-10-30 14:09:38 +0530

    basic learning CSS styling

---
+++ src/pages/check.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Link } from 'gatsby'
+import Layout from '../components/Layout'
+import styled from 'styled-components'
+
+const check = () => {
+  return (
+    <Layout>      
+      <Wrapper>
+        <h1>check</h1>
+        <Link to="/">Home</Link>
+        <p className='text'>This is to check. Lorem23gf Lorem23gf Lorem23gf Lorem23gf</p>
+      </Wrapper>
+    </Layout>
+  )
+}
+
+const Wrapper = styled.section`
+  
+color:green;
+
+h1{
+  color: blueviolet;
+}
+
+.text{
+  text-transform: capitalize;
+}
+`
+
+export default check
