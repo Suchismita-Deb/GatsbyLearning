bbcbd8fefe809d0a21b93b36de6ade320e864a75
Suchismita-Deb <suchismitadeb2000@gmail.com>
2023-11-01 20:43:31 +0530

    base template of a website

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import Layout from '../components/Layout'
 
-const Check = () => {
+const About = () => {
   return (
     <Layout>      
-      <h1>Check Page</h1>
+      <h1>About Page</h1>
     </Layout>
   )
 }
 
-export default Check
+
+export default About
