dea1cff64b7c777ef68aacc8ba6e060f736b6ce1
Suchismita-Deb <suchismitadeb2000@gmail.com>
2023-11-04 21:41:44 +0530

    static image

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,6 +9,9 @@
  */
 module.exports = {
   plugins: [
-    `gatsby-plugin-styled-components`
+    `gatsby-plugin-styled-components`,
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
   ],
 }
