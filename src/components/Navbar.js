f400888d98900469cb421f3791dd416263d47327
Suchismita-Deb <suchismitadeb2000@gmail.com>
2023-11-04 15:56:01 +0530

    navbar toggle

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,27 +1,32 @@
-import React from 'react'
+import React,{useState} from 'react'
 import { Link } from 'gatsby'
 import {FiAlignJustify} from 'react-icons/fi'
 import logo from '../assets/images/logo.svg'
 
 const Navbar = () => {
+    const [show,setShow] = useState(false);
   return (
     <nav className='navbar'>
         <div className='nav-center'>
+
             <div className='nav-header'>
                 {/* This is the pace where the image and the icon will be present. */}
                 <Link to="/">
                     <img src={logo} alt="logo"/>
                 </Link>
-                <button className='nav-btn'>
+
+                <button className='nav-btn' onClick={() => setShow(!show)}>
                     <FiAlignJustify/>
                 </button>
             </div> 
-            <div className='nav-links show-links'>
+
+            <div className={show?"nav-links show-links":"nav-links"}>
                 {/* This is the place which will toggle with the small screen. */}
-                <Link to='/' className='nav-link' activeClassName='active-link'>home</Link>
-                <Link to='/recipes' className='nav-link' activeClassName='active-link'>recipes</Link>
-                <Link to='/tags' className='nav-link' activeClassName='active-link'>tags</Link>
+                <Link to='/' className='nav-link' activeClassName='active-link' onClick={() => setShow(false)}>home</Link>
+                <Link to='/recipes' className='nav-link' activeClassName='active-link' onClick={() => setShow(false)}>recipes</Link>
+                <Link to='/tags' className='nav-link' activeClassName='active-link' onClick={() => setShow(false)}>tags</Link>
                 <Link to='/about' className='nav-link' activeClassName='active-link'>about</Link>
+                {/* setShow(false) It can be only clicked when the navbar is visible.*/}
 
                 <div className='nav-link contact-link'>
                     <Link to="/contact" className='btn'>
