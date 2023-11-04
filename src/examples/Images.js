b496357cb87dd925d9050d3c86af5b3ad790d647
Suchismita-Deb <suchismitadeb2000@gmail.com>
2023-11-04 21:49:21 +0530

    static image contrained

--- src/examples/Images.js
+++ src/examples/Images.js
@@ -7,14 +7,31 @@ import {StaticImage} from "gatsby-plugin-image"
 const Images = () => {
   return (
     <Wrapper>
-        {/* <img src={picture} alt='Image'></img> */}
-        <StaticImage src='../assets/images/picture.jpg' alt='Image'/>        
+        {/* <img src={picture} alt='Image'></img>
+        <StaticImage src='../assets/images/picture.jpg' alt='Image'/> */}
+        <article>
+            <h5>Contrained/Default</h5>
+            <StaticImage 
+                src='../assets/images/recipe-1.jpeg' 
+                alt='Image' 
+                height={400} 
+                placeholder='tracedSVG' 
+                layout='constrained'
+                className="example-img"
+                as="section"
+            />
+        </article>
     </Wrapper>
   )
 }
 const Wrapper = styled.section`
-img{
-    width:300px;
+width:70vw;
+margin: 0 auto;
+article{
+    border:2px solid red;
+}
+.example-img{
+    border-radius:1rem;
 }
 `
 
