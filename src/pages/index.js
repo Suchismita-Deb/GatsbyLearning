6ab5b1c488aaf471ba0f6d8a64130d605add82da
Suchismita-Deb <suchismitadeb2000@gmail.com>
2023-11-05 19:44:55 +0530

    about page added

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,12 @@
 import * as React from "react"
 import Layout from "../components/Layout"
-import Images from "../examples/Images"
+// import Images from "../examples/Images"
 
 export default function Home() {
   return (
   <Layout>
     <h1>Home Page</h1>
-    <Images/>
+    {/* <Images/> */}
   </Layout>
   )
 }
