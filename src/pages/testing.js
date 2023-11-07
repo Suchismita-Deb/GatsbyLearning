d45b6c693e9085dc3212304b58f0483389f791ca
Suchismita-Deb <suchismitadeb2000@gmail.com>
2023-11-07 23:06:48 +0530

    testing file added gatsby

---
+++ src/pages/testing.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import { graphql } from 'gatsby'
+
+const Testing = props => {
+    console.log(props)
+  return (
+    <div>Testing</div>
+  )
+}
+
+export default Testing
